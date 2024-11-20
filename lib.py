17b8d7da266c3225516c6eda1c31a0cecb1dde80
студент211 <211@mveu.local>
2024-11-20 09:06:00 +0400

    refactoring

--- lib.py
+++ lib.py
@@ -1,6 +1,77 @@
+from string import ascii_uppercase
+import openpyxl.styles
+
 def to_power_symbol(x):
     superscript_digits = {
         0: '⁰', 1: '¹', 2: '²', 3: '³', 4: '⁴', 5: '⁵', 
         6: '⁶', 7: '⁷', 8: '⁸', 9: '⁹'
     }
     return ''.join(superscript_digits[int(digit)] for digit in str(x))
+
+def get_filename_ext(filename: str, ext: str):
+    return f'./excel/{filename}.{ext}'
+
+def get_delta(y: list):
+    deltas = []
+    while len(y) > 1:
+        next_deltas = []
+        for i in range(len(y) - 1):
+            result = y[i + 1] - y[i]
+            next_deltas.append(result)
+        deltas.append(next_deltas)
+        y = next_deltas
+    return deltas
+
+def render_lagrange_table(ws, x_table: list[int], y_table: list[int], func):
+    ws['A1'] = 'x'
+    for i in range(len(x_table)):
+        ws[f'A{i + 2}'] = x_table[i]
+    ws['B1'] = 'y'
+    for i in range(len(y_table)):
+        ws[f'B{i + 2}'] = y_table[i]
+
+    for i in ws[f'A1:B1']:
+        for j in i:
+            j.fill = openpyxl.styles.PatternFill(fill_type='solid', start_color='f8ff7a')
+            
+    ws[f'A{len(x_table) + 3}'] = "f(x) ="
+    ws[f'B{len(x_table) + 3}'] = str(func)
+
+def render_newton_table(ws, x_table: list[int], y_table: list[int], all_deltas: list[list[int]], func):
+    ws['A1'] = 'x'
+    for i in range(len(x_table)):
+        ws[f'A{i + 2}'] = x_table[i]
+    ws['B1'] = 'y'
+    for i in range(len(y_table)):
+        ws[f'B{i + 2}'] = y_table[i]
+
+    for i in range(len(x_table) - 1):
+        column = ascii_uppercase[i + 2]
+        cell = f"{column}1"
+        ws[cell] = f'Δ{to_power_symbol(i + 1)}y'
+        for j in range(len(all_deltas[i])):
+            row = j + 2
+            column = ascii_uppercase[i + 1]
+            if (ws[f'{column}{row + 1}'].value and ws[f'{column}{row}'].value):
+                ws[f"{ascii_uppercase[i + 2]}{j + 2}"] = f"={column}{row + 1}-{column}{row}"
+
+    for i in ws[f'A1:{ascii_uppercase[len(x_table)]}1']:
+        for j in i:
+            j.fill = openpyxl.styles.PatternFill(fill_type='solid', start_color='f8ff7a')
+
+    ws[f'A{len(x_table) + 3}'] = "f(x) ="
+    ws[f'B{len(x_table) + 3}'] = str(func)
+
+    ws[f'B{len(x_table) + 3}'].fill = openpyxl.styles.PatternFill(fill_type='solid', start_color='58fc84')
+    ws[f'A{len(x_table) + 3}'].fill = openpyxl.styles.PatternFill(fill_type='solid', start_color='58fc84')    
+
+def render_vba_function(wb, func):
+    f = str(func).replace('**', ' ^ ')
+    vba_code = f'''
+    Function Polinom(x As Double) As Double
+        Polinom = {f}
+    End Function
+    '''
+    
+    vba_module = wb.api.VBProject.VBComponents.Add(1)
+    vba_module.CodeModule.AddFromString(vba_code)
--- main.py
+++ main.py
@@ -1,86 +1,57 @@
 import os
-import openpyxl
 import xlwings
-from string import ascii_uppercase
-import openpyxl.styles
-from lib import to_power_symbol
-from sympy import interpolate, simplify
+import openpyxl
+from lib import get_filename_ext, get_delta, render_lagrange_table, render_newton_table, render_vba_function
+from sympy import interpolate
 from sympy.abc import x
-
-filename = input('Filename: ')
 
 if not os.path.isdir('./excel'):
     os.mkdir('./excel')
 
-data = {
-    'x': [1, 2, 3],
-    'y': [1, 4, 9]
+filename = input('Filename: ')
+
+print('1 - Метод Лагранжа')
+print('2 - Метод Ньютона')
+method = int(input('Method: '))
+if method > 2 or method < 1:
+    print('Неверно введен метод')
+    os._exit(1)
+
+xlsx_filename = get_filename_ext(filename, 'xlsx')
+xlsm_filename = get_filename_ext(filename, 'xlsm')
+
+table = {
+    'x': [1, 2, 3, 5],
+    'y': [1, 4, 9, 25]
 }
 
-x_table = data.get('x')
-y_table = data.get('y')
-
-def get_delta(y: list):
-    deltas = []
-    while len(y) > 1:
-        next_deltas = []
-        for i in range(len(y) - 1):
-            result = y[i + 1] - y[i]
-            next_deltas.append(result)
-        deltas.append(next_deltas)
-        y = next_deltas
-    return deltas
+x_table = table.get('x')
+y_table = table.get('y')
 
 all_deltas = get_delta(y_table)
 
 func = interpolate([(x_table[i], y_table[i]) for i in range(len(x_table))], x)
-    
+
 wb = openpyxl.Workbook()
 ws = wb.active
 ws.title = filename
 
-ws['A1'] = 'x'
-for i in range(len(x_table)):
-    ws[f'A{i + 2}'] = x_table[i]
-ws['B1'] = 'y'
-for i in range(len(y_table)):
-    ws[f'B{i + 2}'] = x_table[i]
+if method == 1:
+    render_lagrange_table(ws, x_table, y_table, func)
+elif method == 2:
+    render_newton_table(ws, x_table, y_table, all_deltas, func)
 
-for i in range(len(x_table) - 1):
-    column = ascii_uppercase[i + 2]
-    cell = f"{column}1"
-    ws[cell] = f'Δ{to_power_symbol(i + 1)}y'
-    for j in range(len(all_deltas[i])):
-        row = j + 2
-        column = ascii_uppercase[i + 1]
-        if (ws[f'{column}{row + 1}'].value and ws[f'{column}{row}'].value):
-            ws[f"{ascii_uppercase[i + 2]}{j + 2}"] = f"={column}{row + 1}-{column}{row}"
-
-for i in ws[f'A1:{ascii_uppercase[len(x_table)]}1']:
-    for j in i:
-        j.fill = openpyxl.styles.PatternFill(fill_type='solid', start_color='f8ff7a')
-
-ws[f'A{len(x_table) + 3}'] = "f(x) ="
-ws[f'B{len(x_table) + 3}'] = str(func)
-
-ws[f'B{len(x_table) + 3}'].fill = openpyxl.styles.PatternFill(fill_type='solid', start_color='58fc84')
-ws[f'A{len(x_table) + 3}'].fill = openpyxl.styles.PatternFill(fill_type='solid', start_color='58fc84')
-
-wb.save(f'./excel/{filename}.xlsx')
+wb.save(xlsx_filename)
+wb.close()
 
 with xlwings.App(visible=True) as app:
-    wb = app.books.open(f'./excel/{filename}.xlsx') 
+    wb = app.books.open(xlsx_filename) 
     ws = wb.sheets[0]
 
-    f = str(func).replace('**', '^')
-    vba_code = f'''
-    Function F(x As Double) As Double
-        F = {f}
-    End Function
-    '''
+    render_vba_function(wb, func)
     
-    vba_module = wb.api.VBProject.VBComponents.Add(1)  # 1 - это добавление нового модуля
-    vba_module.CodeModule.AddFromString(vba_code)
-    
-    wb.save(r'./excel/custom_function.xlsm')
+    wb.save(xlsm_filename)
     wb.close()
+    
+if os.path.isfile(os.path.abspath(xlsm_filename)):
+    os.remove(os.path.abspath(xlsx_filename))
